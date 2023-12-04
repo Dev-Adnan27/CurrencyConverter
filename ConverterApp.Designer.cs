2fd5461351b865500f721ef24037d4fdbbbb354b
Adnan Hasnain <123970469+Dev-Adnan27@users.noreply.github.com>
2023-12-04 16:24:22 +0500

    Full Project Uploaded

---
+++ ConverterApp.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CurrencyConverter
+{
+    partial class CurrencyConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CurrencyConverter));
+            this.FromConvertCurrency = new System.Windows.Forms.ComboBox();
+            this.FromMoney = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ToConvertCurrency = new System.Windows.Forms.ComboBox();
+            this.ToMoney = new System.Windows.Forms.TextBox();
+            this.from = new System.Windows.Forms.TextBox();
+            this.ShuffleBtn = new System.Windows.Forms.PictureBox();
+            this.ConvertBtn = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ShuffleBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FromConvertCurrency
+            // 
+            this.FromConvertCurrency.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.FromConvertCurrency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FromConvertCurrency.ForeColor = System.Drawing.Color.White;
+            this.FromConvertCurrency.FormattingEnabled = true;
+            this.FromConvertCurrency.Location = new System.Drawing.Point(425, 243);
+            this.FromConvertCurrency.Name = "FromConvertCurrency";
+            this.FromConvertCurrency.Size = new System.Drawing.Size(96, 28);
+            this.FromConvertCurrency.TabIndex = 2;
+            // 
+            // FromMoney
+            // 
+            this.FromMoney.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.FromMoney.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.FromMoney.ForeColor = System.Drawing.Color.White;
+            this.FromMoney.Location = new System.Drawing.Point(184, 243);
+            this.FromMoney.Name = "FromMoney";
+            this.FromMoney.Size = new System.Drawing.Size(206, 19);
+            this.FromMoney.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(634, 69);
+            this.textBox2.TabIndex = 99;
+            this.textBox2.Text = "Currency Converter";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ToConvertCurrency
+            // 
+            this.ToConvertCurrency.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ToConvertCurrency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ToConvertCurrency.ForeColor = System.Drawing.Color.White;
+            this.ToConvertCurrency.FormattingEnabled = true;
+            this.ToConvertCurrency.Location = new System.Drawing.Point(425, 392);
+            this.ToConvertCurrency.Name = "ToConvertCurrency";
+            this.ToConvertCurrency.Size = new System.Drawing.Size(96, 28);
+            this.ToConvertCurrency.TabIndex = 3;
+            // 
+            // ToMoney
+            // 
+            this.ToMoney.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ToMoney.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ToMoney.ForeColor = System.Drawing.Color.White;
+            this.ToMoney.Location = new System.Drawing.Point(184, 394);
+            this.ToMoney.Name = "ToMoney";
+            this.ToMoney.ReadOnly = true;
+            this.ToMoney.Size = new System.Drawing.Size(206, 19);
+            this.ToMoney.TabIndex = 99;
+            // 
+            // from
+            // 
+            this.from.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.from.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.from.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.from.ForeColor = System.Drawing.Color.White;
+            this.from.Location = new System.Drawing.Point(66, 188);
+            this.from.Name = "from";
+            this.from.ReadOnly = true;
+            this.from.Size = new System.Drawing.Size(104, 23);
+            this.from.TabIndex = 99;
+            this.from.Text = "From";
+            // 
+            // ShuffleBtn
+            // 
+            this.ShuffleBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ShuffleBtn.Image = ((System.Drawing.Image)(resources.GetObject("ShuffleBtn.Image")));
+            this.ShuffleBtn.Location = new System.Drawing.Point(277, 297);
+            this.ShuffleBtn.Name = "ShuffleBtn";
+            this.ShuffleBtn.Size = new System.Drawing.Size(92, 75);
+            this.ShuffleBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ShuffleBtn.TabIndex = 4;
+            this.ShuffleBtn.TabStop = false;
+            this.ShuffleBtn.Click += new System.EventHandler(this.ShuffleBtn_Click);
+            // 
+            // ConvertBtn
+            // 
+            this.ConvertBtn.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ConvertBtn.FlatAppearance.BorderSize = 0;
+            this.ConvertBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ConvertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConvertBtn.Location = new System.Drawing.Point(247, 521);
+            this.ConvertBtn.Name = "ConvertBtn";
+            this.ConvertBtn.Size = new System.Drawing.Size(152, 52);
+            this.ConvertBtn.TabIndex = 4;
+            this.ConvertBtn.Text = "Convert";
+            this.ConvertBtn.UseVisualStyleBackColor = false;
+            this.ConvertBtn.Click += new System.EventHandler(this.ConvertBtn_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox6.Location = new System.Drawing.Point(-10, 649);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(679, 39);
+            this.textBox6.TabIndex = 99;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox4.ForeColor = System.Drawing.Color.White;
+            this.textBox4.Location = new System.Drawing.Point(66, 349);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(104, 23);
+            this.textBox4.TabIndex = 99;
+            this.textBox4.Text = "To";
+            // 
+            // CurrencyConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(658, 683);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.ConvertBtn);
+            this.Controls.Add(this.ShuffleBtn);
+            this.Controls.Add(this.from);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.ToMoney);
+            this.Controls.Add(this.FromMoney);
+            this.Controls.Add(this.ToConvertCurrency);
+            this.Controls.Add(this.FromConvertCurrency);
+            this.ForeColor = System.Drawing.Color.Snow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "CurrencyConverter";
+            this.Text = "Currency Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.ShuffleBtn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox FromConvertCurrency;
+        private System.Windows.Forms.TextBox FromMoney;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox ToConvertCurrency;
+        private System.Windows.Forms.TextBox ToMoney;
+        private System.Windows.Forms.TextBox from;
+        private System.Windows.Forms.PictureBox ShuffleBtn;
+        private System.Windows.Forms.Button ConvertBtn;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ ConverterApp.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace CurrencyConverter
+{
+    public partial class CurrencyConverter : Form
+    {
+        public CurrencyConverter()
+        {
+            InitializeComponent();
+            PopulateCurrencyCodesFrom();
+            PopulateCurrencyCodesTo();
+
+        }
+        private async Task<decimal?> GetCurrencyConversion(string fromCurrency, string toCurrency, decimal amount)
+        {
+            string apiUrl = $"https://currency-exchange.p.rapidapi.com/exchange?from={fromCurrency}&to={toCurrency}&q={amount}";
+            string apiKey = "e13b606d9fmshec5c3f24bbed7c4p18b470jsn53af55c1d39e";
+            string apiHost = "currency-exchange.p.rapidapi.com";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-RapidAPI-Key", apiKey);
+                client.DefaultRequestHeaders.Add("X-RapidAPI-Host", apiHost);
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    return ExtractNewAmountFromJson(result);
+                }
+                else
+                {
+                    string error = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show($"Error: {error}");
+                    return null;
+                }
+            }
+        }
+        private decimal? ExtractNewAmountFromJson(string json)
+        {
+            try
+            {
+                JToken token = JToken.Parse(json);
+                if (token.Type == JTokenType.Float || token.Type == JTokenType.Integer)
+                {
+                    decimal newAmount = (decimal)token;
+                    return newAmount;
+                }
+                else
+                {
+                    MessageBox.Show("Unexpected JSON format or missing data.");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error parsing JSON: {ex.Message}");
+                return null;
+            }
+        }
+        private void PopulateCurrencyCodesFrom()
+        {
+            string[] currencyCodes = {
+                 "PKR","USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY",  "SEK",
+                 "KRW", "SGD", "NOK", "MXN", "INR", "RUB", "ZAR", "TRY", "BRL", "HKD",
+                 "DKK", "AED", "ARS", "TWD", "THB", "PLN", "IDR", "ILS", "PHP", "CLP",
+                 "CZK", "EGP", "MYR", "HUF", "COP", "SAR", "VND", "RON", "KES", "NGN",
+                 "UAH", "BDT", "IQD", "MAD", "UZS", "QAR", "HRK", "OMR", "SGD", "NZD",
+                 "DZD", "KZT", "TND", "UGX", "BDT", "LKR", "MOP", "NPR", "SYP", "UYU",
+                 "BYN", "KHR", "GEL", "JOD", "NAD", "RSD", "BHD", "BND", "ERN", "ETB",
+                 "FJD", "GHS", "ISK", "JMD", "KPW", "LBP", "LSL", "LYD", "MKD", "MUR",
+        };
+            FromConvertCurrency.DataSource = currencyCodes;
+        }     
+        private void PopulateCurrencyCodesTo()
+        {
+            string[] currencyCodes = {
+                 "PKR","USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY",  "SEK",
+                 "KRW", "SGD", "NOK", "MXN", "INR", "RUB", "ZAR", "TRY", "BRL", "HKD",
+                 "DKK", "AED", "ARS", "TWD", "THB", "PLN", "IDR", "ILS", "PHP", "CLP",
+                 "CZK", "EGP", "MYR", "HUF", "COP", "SAR", "VND", "RON", "KES", "NGN",
+                 "UAH", "BDT", "IQD", "MAD", "UZS", "QAR", "HRK", "OMR", "SGD", "NZD",
+                 "DZD", "KZT", "TND", "UGX", "BDT", "LKR", "MOP", "NPR", "SYP", "UYU",
+                 "BYN", "KHR", "GEL", "JOD", "NAD", "RSD", "BHD", "BND", "ERN", "ETB",
+                 "FJD", "GHS", "ISK", "JMD", "KPW", "LBP", "LSL", "LYD", "MKD", "MUR",
+        };
+            ToConvertCurrency.DataSource = currencyCodes;
+        }
+
+        private async void ConvertBtn_Click(object sender, EventArgs e)
+        {
+            string selectedFromCurrency = FromConvertCurrency.SelectedItem?.ToString();
+            string selectedToCurrency = ToConvertCurrency.SelectedItem?.ToString();
+            decimal selectedAmount = Convert.ToDecimal(FromMoney.Text);
+            decimal? newAmount = await GetCurrencyConversion(selectedFromCurrency,selectedToCurrency,selectedAmount);
+            if (newAmount != null)
+            {
+                ToMoney.Text = newAmount.ToString();
+            }
+        }
+
+        private void ShuffleBtn_Click(object sender, EventArgs e)
+        {
+            string xyx = ToMoney.Text;
+            ToMoney.Text = FromMoney.Text;
+            FromMoney.Text = xyx;
+            string yxy = FromConvertCurrency.SelectedItem?.ToString();
+            FromConvertCurrency.SelectedItem = ToConvertCurrency.SelectedItem?.ToString();
+            ToConvertCurrency.SelectedItem = yxy;
+        }
+    }
+}
